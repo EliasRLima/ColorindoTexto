d231c6dfc17a73edc442e4fd7e8ecb3dda45d2de
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2020-10-30 10:23:36 -0300

    richtext
    -Trocando a CodeArea para StyledArea

--- src/ControllerPrincipal.java
+++ src/ControllerPrincipal.java
@@ -23,7 +23,7 @@ import javafx.scene.paint.Paint;
 public class ControllerPrincipal implements Initializable{
 
 	@FXML
-	CodeArea texto;
+	StyleClassedTextArea texto;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -139,7 +139,7 @@ public class ControllerPrincipal implements Initializable{
 		
 	}
 	
-	public static CodeArea colorirArea(CodeArea area, int in, int out) {
+	public static StyleClassedTextArea colorirArea(StyleClassedTextArea area, int in, int out) {
 		String texto = "";
 		 try {
 			 texto = area.getText().replace("\t"," ").substring(in,out);
@@ -205,7 +205,7 @@ public class ControllerPrincipal implements Initializable{
 		return area;
 	}
 	
-	public static CodeArea colorirArea(CodeArea area, int in) {
+	public static StyleClassedTextArea colorirArea(StyleClassedTextArea area, int in) {
 		colorirArea(area, in, area.getText().length());
 		
 		return area;
